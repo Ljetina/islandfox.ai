e383cde511b87eb9a376a5eb21c79792a212481c
bartolkaruza <bartol.k@gmail.com>
2023-08-18 08:12:14 +0000

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from 'next';
-
 import { Inter } from 'next/font/google';
+
 // import { Head, Html, Main, NextScript } from 'next/document'
 
 // import './globals.css';
 // import "slick-carousel/slick/slick-theme.css"
 // import "slick-carousel/slick/slick.css"
-
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,8 +25,35 @@ export default async function RootLayout({
         {/* <title>{metadata.title}</title> */}
         {/* <meta name="description" content={metadata.description} /> */}
         {/* <link rel="shortcut icon" href="/assets/img/favicon.png" /> */}
-        {/* <link rel="preconnect" href="https://fonts.bunny.net" /> */}
-        {/* <link href="https://fonts.bunny.net/css?family=outfit:100,200,300,400,500,600,700|playfair-display:400,400i,500,500i,600,600i,700,700i,800,800i|plus-jakarta-sans:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet" /> */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/img/16_favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/img/32_favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="64x64"
+          href="/assets/img/64_favicon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="128x128"
+          href="/assets/img/128_favicon.png"
+        />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+          href="https://fonts.bunny.net/css?family=outfit:100,200,300,400,500,600,700|playfair-display:400,400i,500,500i,600,600i,700,700i,800,800i|plus-jakarta-sans:400,400i,500,500i,600,600i,700,700i,800,800i"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <div className={inter.className}>
